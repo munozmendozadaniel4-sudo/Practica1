5cb222b48211ccfeb8749b632d350bd555febb2a
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-05-06 23:37:39 -0600

    Update Aplicacion.java

--- src/ito/poo/Aplicacion.java
+++ src/ito/poo/Aplicacion.java
@@ -1,4 +1,12 @@
 package ito.poo;
+public class App {
+    static Mascota mascota;
 
-public class Aplicacion {
+    static void run(){
+        mascota= new Mascota("satur",2,"perro","chihuahua");
+        System.out.println(mascota);
+        mascota.cumpleAños();
+        System.out.println("Despues de cumplir años:"+mascota);
+    }
+    public static void main (String[] args){run();}
 }
