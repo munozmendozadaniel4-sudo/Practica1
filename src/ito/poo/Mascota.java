509a1977563f19cec061856b4d9429199dcc0901
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-05-06 23:37:15 -0600

    Update Mascota.java

--- src/ito/poo/Mascota.java
+++ src/ito/poo/Mascota.java
@@ -1,4 +1,34 @@
 package ito.poo;
 
 public class Mascota {
+    private String nombre;
+    private int edad;
+    private String especie;
+    private String raza;
+
+    public Mascota(String nombre, int edad, String especie, String raza){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.raza = raza;
+    }
+    public void cumpleAños(){
+        this.edad++;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public String getEspecie(){
+        return especie;
+    }
+    public String getRaza(){
+        return raza;
+    }
+    @Override
+    public String toString(){
+        return "Mascota:{"+nombre+","+edad+","+especie+","+raza+"}";
+    }
 }
